74e46335403e8329245eb4411819f4e6da3e10f5
artivan <78273357+artivan@users.noreply.github.com>
2021-02-18 15:21:23 -0600

    Modificacion

---
+++ inicio/Assets/Scripts/mesa2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mesa2 : MonoBehaviour
+{
+    public GameObject TextoFlotante;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            //Destroy(gameObject);
+            MostrarTextoFlotante();
+        }
+    }
+
+    public void MostrarTextoFlotante()
+    {
+        GameObject texto = Instantiate(TextoFlotante);
+        texto.transform.position = new Vector3(this.gameObject.transform.position.x +4 ,
+            this.gameObject.transform.position.y + 2,
+            this.gameObject.transform.position.z - 10);
+        texto.transform.Rotate(0.0f, 180.0f, 0.0f, Space.Self);
+    }
+}
---
+++ inicio/Assets/Scripts/personajeN1.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class personajeN1 : MonoBehaviour
+{
+    public float vel = 1.0f;
+    private Animator anim;
+    public float x, y;
+    public GameObject TextoFlotante;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("vx", x);
+        anim.SetFloat("vy", y);
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            //Destroy(gameObject);
+            MostrarTextoFlotante();
+        }
+    }
+
+    public void MostrarTextoFlotante()
+    {
+        GameObject texto = Instantiate(TextoFlotante);
+        texto.transform.position = new Vector3(this.gameObject.transform.position.x+8,
+            this.gameObject.transform.position.y + 2,
+            this.gameObject.transform.position.z - 10);
+        texto.transform.Rotate(0.0f, 160.0f, 0.0f, Space.Self);
+    }
+}
